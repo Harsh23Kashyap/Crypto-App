85b2432608b03705f6326a15127bc8149ebd67fd
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-03-14 16:09:57 +0530

    Delete Development/Weather 2.0 directory

--- Development/Weather 2.0/android/app/src/main/kotlin/com/example/flutter_clima/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_clima
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Development/Weather 2.0/lib/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-part 'weather_image.dart';
-part 'reusable_card.dart';
